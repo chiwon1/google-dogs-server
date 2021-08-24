de724c9aab67e751aefd964fa8f76b90d6a2bbfc
Chiwon <emphasize1@gmail.com>
2021-08-24 18:08:31 +0900

    Feat: set document schema
     -  models/Document.js

--- app.js
+++ app.js
@@ -65,15 +65,15 @@ app.io.on("connection", socket => {
   socket.on("get-document", async documentId => {
     const document = await findOrCreateDocument(documentId)
     socket.join(documentId)
-    socket.emit("load-document", document.data)
+    socket.emit("load-document", document.body)
 
     socket.on("send-changes", delta => {
       console.log("delta", delta);
       socket.broadcast.to(documentId).emit("receive-changes", delta)
     })
 
-    socket.on("save-document", async data => {
-      await Document.findByIdAndUpdate(documentId, { data })
+    socket.on("save-document", async body => {
+      await Document.findByIdAndUpdate(documentId, { body })
     })
   })
 });
@@ -83,7 +83,7 @@ async function findOrCreateDocument(id) {
 
   const document = await Document.findById(id)
   if (document) return document
-  return await Document.create({ _id: id, data: defaultValue })
+  return await Document.create({ _id: id, body: defaultValue })
 }
 
 module.exports = app;
--- models/Document.js
+++ models/Document.js
@@ -1,8 +1,29 @@
 const mongoose = require("mongoose");
 
 const documentSchema = new mongoose.Schema({
-  _id: String,
-  data: Object,
+  _id: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    maxLength: 50,
+    required: true,
+  },
+  body: {
+    type: String,
+    maxLength: 300,
+  },
+  creator: {
+    type: String,
+    maxLength: 50,
+    required: true,
+  },
+  collaborator: {
+    type: Array,
+  },
+}, {
+  timestamps: true,
 });
 
 module.exports = mongoose.model("Document", documentSchema);
