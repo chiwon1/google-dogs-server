ad2481010e2c3207f13a0e33e3385fef13dacd6d
Chiwon <emphasize1@gmail.com>
2021-08-25 13:26:31 +0900

    Refactor: add error handlings

--- routes/controllers/documents.controller.js
+++ routes/controllers/documents.controller.js
@@ -9,18 +9,34 @@ const ERROR = require("../../constants/errorConstants");
 exports.create = async function (req, res, next) { };
 
 exports.get = async function (req, res, next) {
-  const documents = await Document.find({ creator: req.user.uid });
+  try {
+    const documents = await Document.find({ creator: req.user.uid });
 
-  res.send({ documents });
+    res.send({ documents });
+  } catch (err) {
+    next(err);
+  }
 };
 
 exports.modify = async function (req, res, next) {
   const _id = req.params._id;
   const body = req.body.body;
 
-  await Document.updateOne({ _id }, { body });
+  try {
+    if (!mongoose.Types.ObjectId.isValid(_id)) {
+      throw createError(400, ERROR.INVALID_ACCESS);
+    }
 
-  res.sendStatus(200);
+    await Document.updateOne({ _id }, { body });
+
+    res.sendStatus(200);
+  } catch (err) {
+    if (err instanceof mongoose.Error.ValidationError) {
+      return next(createError(400, ERROR.INVALID_DATA));
+    }
+
+    next(err);
+  }
 };
 
 exports.delete = async function (req, res, next) {
